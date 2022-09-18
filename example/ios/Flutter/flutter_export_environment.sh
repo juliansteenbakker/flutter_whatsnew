ff2a79d64bc332d66217104cba59df5dc365b9b9
Rody Davis <rody.davis.jr@gmail.com>
2022-09-17 20:40:20 -0700

    updating deps

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/1.2.1/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/rodydavis/Developer/GitHub/plugins/packages/flutter_whatsnew/example"
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/2.10.4/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/rodydavis/Developer/hymns-for-worship/app/packages/whatsnew/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
